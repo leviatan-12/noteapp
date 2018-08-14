4d7c4b1fdffe98bd99c98ce2d4774e4af2176b70
= <=>
2018-08-14 13:43:03 -0500

    Adding static webpages to the responses

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate actix_web;
 
-use actix_web::{App, server, HttpRequest, http::{Method}, HttpResponse};
+use actix_web::{App, server, HttpRequest, http::{Method, StatusCode}, fs, Result};
 
-fn index_get(_req: &HttpRequest) -> &'static str {
-    "Hola peticion get"
+fn index_get(_req: &HttpRequest) -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/index.html")?)
 }
 
 
@@ -11,8 +11,8 @@ fn index_post(_req: &HttpRequest) -> &'static str {
     "Hola peticion post"
 }
 
-fn handler_404(_req: &HttpRequest) -> &'static str {
-    "No existe lo que buscas"
+fn handler_404(_req: &HttpRequest) -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/404.html")?.set_status_code(StatusCode::NOT_FOUND))
 }
 
 fn main() {
