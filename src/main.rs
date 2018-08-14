c53c58530b04e47f4b71373a16efaa4aff67eb8f
= <=>
2018-08-14 09:49:05 -0500

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate actix_web;
+
+use actix_web::{App, server, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hola mundo"
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(move || {
+        App::new()
+            .resource("/",|r| r.f(index))
+    })
+    .bind("127.0.0.1:8000")
+    .unwrap()
+    .run()
 }
